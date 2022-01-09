f1d05c5c371366769d3dc57b6de26d8fc3a36ef9
amobeeb <amobeeb1net@gmail.com>
2022-01-09 17:52:37 +0100

    migration done

---
+++ app/Expertise.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Expertise extends Model
+{
+    use HasFactory;
+}
